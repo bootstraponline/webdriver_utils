7334e0269d2184c8edeec9fb891bda6219240390
bootstraponline <code@bootstraponline.com>
2015-06-05 23:06:08 -0400

    Update page_name regex

--- lib/webdriver_utils/page.rb
+++ lib/webdriver_utils/page.rb
@@ -12,7 +12,7 @@ module WebDriverUtils
       page_module.constants.each do |page_class|
         # ButtonsPage => buttons_page
         # https://github.com/rails/rails/blob/daaa21bc7d20f2e4ff451637423a25ff2d5e75c7/activesupport/lib/active_support/inflector/methods.rb#L96
-        page_name = page_class.to_s.gsub(/([a-z])([A-Z])/, '\1_\2').downcase
+        page_name = page_class.to_s.gsub(/([a-z\d])([A-Z])/, '\1_\2').downcase
         target_class.send(:define_singleton_method, page_name) do
           page_module.const_get(page_class).new driver_object
         end
