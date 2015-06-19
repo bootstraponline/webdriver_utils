764a48dcb01410e156023bf7da3567ab6abda18b
bootstraponline <code@bootstraponline.com>
2015-06-19 14:45:14 -0400

    Release 1.0.0

--- lib/webdriver_utils/version.rb
+++ lib/webdriver_utils/version.rb
@@ -1,4 +1,4 @@
 module WebDriverUtils
-  VERSION = '0.0.6' unless defined? ::WebDriverUtils::VERSION
-  DATE    = '2015-06-07' unless defined? ::WebDriverUtils::DATE
+  VERSION = '1.0.0' unless defined? ::WebDriverUtils::VERSION
+  DATE    = '2015-06-19' unless defined? ::WebDriverUtils::DATE
 end
