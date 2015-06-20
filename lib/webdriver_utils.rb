cbf55173cda55be64fe1ea970c3e3e628bb6b344
bootstraponline <code@bootstraponline.com>
2015-06-20 11:38:16 -0400

    Move page helpers to angular_webdriver

--- lib/webdriver_utils.rb
+++ lib/webdriver_utils.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'selenium-webdriver'
 
-require_relative 'webdriver_utils/page'
 require_relative 'webdriver_utils/sauce'
 require_relative 'webdriver_utils/silence_gem_warnings'
 require_relative 'webdriver_utils/version'
