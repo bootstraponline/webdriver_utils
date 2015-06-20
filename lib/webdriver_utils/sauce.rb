05d369c27435707025ae49477b554254efe00337
bootstraponline <code@bootstraponline.com>
2015-06-20 11:38:40 -0400

    Update sauce helpers

--- lib/webdriver_utils/sauce.rb
+++ lib/webdriver_utils/sauce.rb
@@ -1,17 +1,26 @@
 # top level sauce helper methods
 
+# Returns sauce username if env is set and not empty.
+# If env isn't defined then nil is returned.
 def sauce_user
-  ENV['SAUCE_USERNAME']
+  env = ENV['SAUCE_USERNAME']
+  (env && !env.empty?) ? env : nil
 end
 
+# Returns sauce key if env is set and not empty.
+# If env isn't defined then nil is returned.
 def sauce_key
-  ENV['SAUCE_ACCESS_KEY']
+  env = ENV['SAUCE_ACCESS_KEY']
+  (env && !env.empty?) ? env : nil
 end
 
+# Returns true if both sauce user and sauce key are defined.
 def sauce?
-  !!(sauce_user && sauce_key)
+  sauce_user && sauce_key
 end
 
+# Returns true if env is set and not empty.
 def jenkins?
-  !!ENV['JENKINS_SERVER_COOKIE']
+  env = ENV['JENKINS_SERVER_COOKIE']
+  env && !env.empty?
 end
